1f0ecc1836eb39d6cbae95e5abb6273ebf2ae352
Jhonn <72659574@continental.edu.pe>
2024-09-16 20:19:44 -0500

    Creacion de las rutas del modulo ventas, compras y pagina principal

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Bienvenido a la pagina principal";
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('ventas', function () {
+    return "Bienvenido a la modulo ventas";
+});
+
+Route::get('ventas/registrarVenta', function () {
+    return "En esta pagina se registrara una venta";
+});
+
+Route::get('ventas/registrarDetalleVenta', function () {
+    return "En esta pagina se registrara un detalle de venta";
+});
+
+Route::get('ventas/generarVoucherVenta', function () {
+    return "En esta pagina se creara un voucher de venta";
+});
+
+Route::get('ventas/anularVenta', function () {
+    return "En esta pagina se podra anular una venta";
+});
+
+Route::get('compras', function () {
+    return "Bienvenido a la pagina principal";
+});
+
+Route::get('compras/registrarCompra', function () {
+    return "En esta pagina se registrara una compra";
+});
+
+Route::get('compras/registrarDetalleCompra', function () {
+    return "En esta pagina se registrara un detalle de compra";
+});
+
+Route::get('compras/generarVoucherCompra', function () {
+    return "En esta pagina se creara un voucher de compra";
+});
+
+Route::get('compras/anularCompra', function () {
+    return "En esta pagina se podra anular una venta";
 });
